d86206fb0618b569d401f179248ac5223cd41661
pfernandez <pedrofernbel@gmail.com>
2024-04-28 12:49:18 +0200

    feat: added SecureRoute component to manage route access rights.

---
+++ src/common/SecureRoute/SecureRoute.tsx
@@ -0,0 +1,41 @@
+import { useSelector } from "react-redux";
+import { userData } from "../../app/slices/userSlice";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const enum ProtMode {
+    allowLoggedIn = 'allow-logged-in',
+    allowLoggedOut = 'allow-logged-out',
+    allowLoggedInAdmin = 'allow-logged-in-admin'
+}
+
+interface SecureRouteProps {
+    protMode: ProtMode;
+}
+
+export const SecureRoute = ({ protMode }: SecureRouteProps) => {
+    const { credentials: { token, user } } = useSelector(userData);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        switch (protMode) {
+            case ProtMode.allowLoggedIn:
+                if (!token) {
+                    navigate('/');
+                }
+                break;
+            case ProtMode.allowLoggedOut:
+                if (token) {
+                    navigate('/');
+                }
+                break;
+            case ProtMode.allowLoggedInAdmin:
+                if (!token || !user.role.includes('admin')) {
+                    navigate('/');
+                }
+                break;
+        }
+    }, [protMode, user, token]);
+
+    return <Outlet />;
+}
