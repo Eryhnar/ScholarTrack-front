276aeb8b3ab4e52fb2e8988f020b6f9a3835f68b
pfernandez <pedrofernbel@gmail.com>
2024-04-28 11:20:53 +0200

    feat: created CInput component.

---
+++ src/common/CInput/CInput.tsx
@@ -0,0 +1,36 @@
+import "./CInput.css"
+
+interface CInputProps {
+    className?: string
+    type: string
+    placeholder: string
+    name: string
+    disabled?: boolean
+    value: string
+    onChangeFunction: (e: React.ChangeEvent<HTMLInputElement>) => void
+    min?: string
+}
+
+export const CInput = ({
+  className = "default-custom-input",
+  type,
+  placeholder,
+  name,
+  disabled = false,
+  value,
+  onChangeFunction,
+  min,
+}: CInputProps) => {
+  return (
+    <input
+      className={className}
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      disabled={disabled}
+      value={value}
+      onChange={onChangeFunction}
+      min={min}
+    />
+  );
+};
