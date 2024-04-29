fb872712859b970595fe760ecac5cd9bde359bbc
pfernandez <pedrofernbel@gmail.com>
2024-04-29 13:21:36 +0200

    feat: basic structure of register view created.

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Toast } from "../../common/Toast/Toast";
+import { CCard } from "../../common/CCard/CCard";
+import { CButton } from "../../common/CButton/CButton";
+import { InfoButton } from "../../common/InfoButton/InfoButton";
+import { CInput } from "../../common/CInput/CInput";
+import "./Register.css";
 
 interface NewUser {
     userName: string;
@@ -14,7 +19,7 @@ interface ErrorMsg {
     emailError: string;
     passwordError: string;
     confirmPasswordError: string;
-    serverError: {message: string, success: boolean};
+    serverError: { message: string, success: boolean };
 }
 
 export const Register: React.FC = (): JSX.Element => {
@@ -32,7 +37,7 @@ export const Register: React.FC = (): JSX.Element => {
         emailError: "",
         passwordError: "",
         confirmPasswordError: "",
-        serverError: {message: "", success: false},
+        serverError: { message: "", success: false },
     });
     const [errorCount, setErrorCount] = useState<number>(0);
 
@@ -47,6 +52,10 @@ export const Register: React.FC = (): JSX.Element => {
     // const validateInput 
 
     //register user
+    const registerUser = async (): Promise<void> => {
+        console.log("registering user");
+    };
+        
 
     return (
         <div className="register-design">
@@ -64,9 +73,81 @@ export const Register: React.FC = (): JSX.Element => {
                     }
                 />
             )}
-            <div>
-                hi
-            </div>
+            <CCard
+                title="Register"
+                content={
+                    <div className="register-inputs">
+                        <div className="register-field">
+                            <div className="register-input-area">
+                                <p>Name</p>
+                                <CInput
+                                    className={`register-handle-field ${errorMsg.userNameError ? "register-field-error" : ""}`}
+                                    type="name"
+                                    placeholder="John"
+                                    name="userName"
+                                    value={newUser.userName || ""}
+                                    onChangeFunction={(e) => inputHandler(e)}
+                                />
+                                <div className="info-button-wrapper"><InfoButton info={"Handle must be between 3 and 20 characters. Can contain lower case letters and numbers. Can contain . _ and - but not consecutively. "} /></div>
+                            </div>
+                            <div className={errorMsg.userNameError ? "register-field-error-msg" : "register-empty-error"}>{errorMsg.userNameError}</div>
+
+                        </div>
+                        <div className="register-field">
+                            <div className="register-input-area">
+                                <p>Email</p>
+                                <CInput
+                                    className={`register-email-field ${errorMsg.emailError ? "register-field-error" : ""}`}
+                                    type="email"
+                                    placeholder="user@domain.com"
+                                    name="email"
+                                    value={newUser.email || ""}
+                                    onChangeFunction={(e) => inputHandler(e)}
+                                />
+                            </div>
+                            <div className={errorMsg.emailError ? "register-field-error-msg" : "register-empty-error"}>{errorMsg.emailError}</div>
+                        </div>
+                        <div className="register-field">
+                            <div className="register-input-area">
+                                <p>Password</p>
+                                <CInput
+                                    className={`register-password-field ${errorMsg.passwordError ? "register-field-error" : ""}`}
+                                    type="password"
+                                    placeholder="password"
+                                    name="password"
+                                    value={newUser.password || ""}
+                                    onChangeFunction={(e) => inputHandler(e)}
+                                />
+                                <div className="info-button-wrapper"><InfoButton info={"Password must be between 8 and 16 character. Must contain a lower case letter, a capital letter and a number. Can contain . _ - but not consecutively."} /></div>
+                            </div>
+                            <div className={errorMsg.passwordError ? "register-field-error-msg" : "register-empty-error"}>{errorMsg.passwordError}</div>
+                        </div>
+                        <div className="register-field">
+                            <div className="register-input-area">
+                                <p>Confirm Password</p>
+                                <CInput
+                                    className={`register-confirm-password-field ${errorMsg.confirmPasswordError ? "register-field-error" : ""}`}
+                                    type="password"
+                                    placeholder="password"
+                                    name="confirmPassword"
+                                    value={newUser.confirmPassword || ""}
+                                    onChangeFunction={(e) => inputHandler(e)}
+                                />
+                            </div>
+                        </div>
+                        <div className={errorMsg.confirmPasswordError ? "register-field-error-msg" : "register-empty-error"}>{errorMsg.confirmPasswordError}</div>
+                        <CButton
+                            className="register-button"
+                            title="Register"
+                            onClickFunction={registerUser}
+                        />
+                        <div className="register-redirect-text">
+
+                            Already registered? Click&nbsp; <a href="/login" className="register-redirect-link">here</a> &nbsp;to log in!
+                        </div>
+                    </div>
+                }
+            />
         </div>
     )
 }
