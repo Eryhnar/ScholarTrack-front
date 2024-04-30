6c3a5fc1b14d268d843913f2527f41940d989536
pfernandez <pedrofernbel@gmail.com>
2024-04-30 11:53:40 +0200

    feat: created basic Home view.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 
+import { NavButton } from "../../common/NavButton/NavButton";
 import "./Home.css";
 
 export const Home: React.FC = (): JSX.Element => {
     return (
         <div className="home-design">
-            <h1>Home</h1>
+            <div className="home-button-wrapper">
+                <NavButton className="home-button" title="Register" path="/register" />
+                <NavButton className="home-button" title="Register" path="/register" />
+                <NavButton className="home-button" title="Register" path="/register" />
+                <NavButton className="home-button" title="Register" path="/register" />
+            </div>
         </div>
     )
 }
