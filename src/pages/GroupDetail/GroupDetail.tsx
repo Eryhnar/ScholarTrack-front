45190cb51dc65b8f4f62578bbef6fc3a3cc294d4
pfernandez <pedrofernbel@gmail.com>
2024-05-05 19:30:46 +0200

    feat: added conditional fetching to GroupDetail if the group in param is not the one in the store.

--- src/pages/GroupDetail/GroupDetail.tsx
+++ src/pages/GroupDetail/GroupDetail.tsx
@@ -1,27 +1,48 @@
-import { useSelector } from "react-redux"
-import { selectGroup } from "../../app/slices/groupDetailSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { selectGroup, setGroup } from "../../app/slices/groupDetailSlice"
 import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import { selectUser } from "../../app/slices/userSlice";
+import { getGroupService } from "../../services/apicalls";
+import { useQuery } from "react-query";
+import { useState } from "react";
 
 export const GroupDetail: React.FC = (): JSX.Element => {
-    const groupId = useParams<{id: string}>().id
+    const token = useSelector(selectUser).credentials.token;
+    const groupId = useParams<{ id: string }>().id!;
     const group = useSelector(selectGroup);
+    const dispatch = useDispatch();
+    const [errorMsg, setErrorMsg] = useState({
+        serverError: { message: "", success: false }
+    })
 
-    useEffect(() => {
-        if (!group || group._id !== groupId) {
-            console.log("fetching group");
-            
-        }
-    },[groupId, group]);
+    console.log("group", group);
+
+    const { data: fetchedGroup, isLoading } = useQuery(['group', groupId], () => getGroupService({ token, groupId }), {
+        enabled: !group || group._id !== groupId,
+        onSuccess: (data) => {
+            dispatch(setGroup(data));
+        },
+        onError: (error: any) => setErrorMsg({
+            serverError: { message: error.message, success: false }
+        })
+    });
+
+    if (isLoading) return <div>Loading...</div>
 
     return (
         <>
             {group && group._id === groupId ?
-            <div className="group-detail">
-                <h1>{group.name}</h1>
-                <h2>{group.level}</h2>
-            </div>
-            : <div className="group-detail">Loading...</div>
+                <div className="group-detail">
+                    <h1>{group.name}</h1>
+                    <h2>{group.level}</h2>
+                </div>
+                // : <div className="group-detail">Loading...</div>
+                : (fetchedGroup &&
+                    <div className="group-detail">
+                        <h1>{fetchedGroup.name}</h1>
+                        <h2>{fetchedGroup.level}</h2>
+                    </div>
+                )
             }
         </>
     )
