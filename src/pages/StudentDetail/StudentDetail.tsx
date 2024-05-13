e9db90f560d1539fbbc58d97400117184084b2df
pfernandez <pedrofernbel@gmail.com>
2024-05-13 13:20:45 +0200

    fix: fixed overflow issue in StudentDetail view.

--- src/pages/StudentDetail/StudentDetail.tsx
+++ src/pages/StudentDetail/StudentDetail.tsx
@@ -25,36 +25,38 @@ export const StudentDetail: React.FC = (): JSX.Element => {
             <CreateButton action={() => navigate(`/groups/${groupId}/${student?._id}/create`)} />
             {student ?
                 <div className="student-detail-wrapper">
-                    <h1>{`${student.name} ${student?.surname}`}</h1>
-                    <div className="student-detail-row">
-                        <p>Age: </p>
-                        <p>{student.age}</p>
-                    </div>
-                    <div className="student-detail-row">
-                        <p>Final Marks: </p>
-                        <p>{student.totalMarks}</p>
-                    </div>
-                    <div className="student-detail-row">
-                        <p>Total Attendance: </p>
-                        <p>{student.totalAttendance}</p>
-                    </div>
-                    {/* <div>
-                    <h2>Attendance</h2>
-                    {student.attendances.map((day) => (
-                        <div key={day._id}>
-                            <p>{day.date}</p>
-                            <p>{day.present ? "Present" : "Absent"}</p>
+                        <h1>{`${student.name} ${student?.surname}`}</h1>
+                    <div className="student-detail-scrollable">
+                        <div className="student-detail-row">
+                            <p>Age: </p>
+                            <p>{student.age}</p>
                         </div>
-                    ))}
-                </div> */}
-                    <div className="student-detail-marks-container">
-                        <h2>Marks</h2>
-                        {student.marks.map((mark) => (
-                            <div key={mark._id} className="student-task-row">
-                                <p>{mark.task[0].name}</p>
-                                <p>{mark.value}</p>
+                        <div className="student-detail-row">
+                            <p>Final Marks: </p>
+                            <p>{student.totalMarks}</p>
+                        </div>
+                        <div className="student-detail-row">
+                            <p>Total Attendance: </p>
+                            <p>{student.totalAttendance}</p>
+                        </div>
+                        {/* <div>
+                        <h2>Attendance</h2>
+                        {student.attendances.map((day) => (
+                            <div key={day._id}>
+                                <p>{day.date}</p>
+                                <p>{day.present ? "Present" : "Absent"}</p>
                             </div>
                         ))}
+                                        </div> */}
+                        <div className="student-detail-marks-container">
+                            <h2>Marks</h2>
+                            {student.marks.map((mark) => (
+                                <div key={mark._id} className="student-task-row">
+                                    <p>{mark.task[0].name}</p>
+                                    <p>{mark.value}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
                 : <h1>Student not found</h1>
